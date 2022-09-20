7384ac371b0805f23cea2a837bb91d06ad886cbf
Shreyansh-09 <2509shreyansh@gmail.com>
2022-09-20 13:23:49 +0530

    done: mark-14

--- app.js
+++ app.js
@@ -13,14 +13,14 @@ function calculateProfitandLoss(initial, quantity,current){
         var profit = (current - initial)*quantity;
         var profitPercentage = (profit/initial)*100;
 
-        showOutput('Hey, the profit is ${profit} and the percent is ${profitPercentage}%.');
+        showOutput(`Hey, the profit is ${profit} and the percent is ${profitPercentage}%.`);
     }
 
     else if(initial > current){
         var loss = (initial - current)*quantity;
         var lossPercentage = (loss/initial)*100;
 
-        showOutput('Hey, the loss is ${loss} and the percent is ${lossPercentage}%.');
+        showOutput(`Hey, the loss is ${loss} and the percent is ${lossPercentage}%.`);
     }
     else{
         showOutput("No pain no gain and no gain no pain.");
